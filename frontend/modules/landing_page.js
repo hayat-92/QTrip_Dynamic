2604b423601ff86850c11612fee74a7a7e35d0ef
hayat-khanum92 <hayat.khanum92@gmail.com>
2021-06-27 20:37:55 +0530

    ME_QTRIPDYNAMIC_MODULE_CITIES

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -15,12 +15,45 @@ async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
 
+  try {
+    let url = `${config.backendEndpoint}/cities`;
+    let response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    return null;
+  }
+
+
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let colm=document.createElement("div");
+  colm.setAttribute("class", "col-6 col-md-3 mb-4");
+  let tile=document.createElement("div");
+  tile.setAttribute("class", "tile");
+  let imge=document.createElement("img");
+  imge.setAttribute("class", "img-fluid");
+  imge.setAttribute("src", image);
+  tile.appendChild(imge);
+  let txt=document.createElement("div");
+  txt.setAttribute("class", "tile-text");
+  let hed=document.createElement("h5");
+  hed.innerHTML=city;
+  let para=document.createElement("p");
+  para.innerHTML=description;
+  txt.appendChild(hed);
+  txt.appendChild(para);
+  tile.appendChild(txt);
+  let lnk=document.createElement("a");
+  let url=`pages/adventures/?city=${id.toLowerCase()}`;
+  lnk.setAttribute("href", url);
+  lnk.setAttribute("id", id);
+  lnk.appendChild(tile);
+  colm.appendChild(lnk);
+  document.getElementById("data").appendChild(colm);
 
 }
 
